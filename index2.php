c4e3378ed303dba0278839d6b4f915ee633beb1c
Rupam <rupamjha323@gmail.com>
2021-02-13 08:08:02 -0500

    put some animation

--- index2.php
+++ index2.php
@@ -26,30 +26,58 @@
 
 
 
-<h1 class="h">CANARA BANK</h1>
+<h1 class="h">CANARA BANK <i class="fas fa-user-tie fa-x fa-spin" style="color:white;"></i></h1>
 
 <div class="container bg-dark outer">
    <p>CANARA BANK</p>
     <P>(MAKE YOUR TRUST ALWAYS)</P>
     <button type="button" class="btn  btn-outline-primary btn-lg bt1"  data-target="#mymodel2"  data-toggle="modal">CREATE ACCOUNT</button>
-
-
-
-
-     <button type="button" class="btn btn-light btn-lg bt2"   data-target="#mymodel" data-toggle="modal">LOGIN</button>
+    <button type="button" class="btn btn-light btn-lg bt2"   data-target="#mymodel" data-toggle="modal">LOGIN</button>
 
 
 </div>
 <div class="outer2">
-
-
 <div class="inner">
-<h1 class="h3">fgvf</h1>
+<h1 class="h3">ABOUT US</h1>
+<table>
+    <tr>
+        <td><div clss="div1">
+
+        <img src="f1.png" alt="erropr" class="im1">
+        </div></td>
+        
+        <td><div clss="div1">
+        <img src="f2.jpg" alt="erropr" class="im1">
+        </div></td>
+    </tr>
+    <br>
+
+    <tr>
+        <td><div clss="div1">
+        <br><br><br>
+        </div></td>
+        
+        <td><div clss="div1">
+       <br><br><br>
+        </div></td>
+    </tr>
+    <tr>
+        <td><div clss="div1">
+        <img src="f3.jpg" alt="erropr" class="im1">
+        </div></td>
+        
+        <td><div clss="div1">
+        <img src="f4.png" alt="erropr" class="im1">
+        </div></td>
+    </tr>
+
+    
+</table>
+
 </div>
 </div>
 
 <div class="footer">
-
 <p class="p1">Location</p>
 <p>Corporate Office: Plot No 4,</p>
 <p> Sector -10 Dwarka New Delhi -110075 </p>
@@ -57,6 +85,8 @@
 
 <p>Branch manager no-9867452309</p>
 <p>trustful bank</p>
+<p><a href=""><i class="fab fa-facebook-square fa-2x fa-spin" style="color:#0033FF;"></i></a> &nbsp;<a href=""><i class="fab fa-twitter-square fa-2x fa-spin " style="color:#0033FF;"></i></a> &nbsp;<a href=""><i class="fab fa-whatsapp-square fa-2x fa-spin" style="color:#00FF00;"></i></a>
+ &nbsp;<a href=""><i class="fab fa-instagram-square fa-2x" style="color:#0033FF";></i></a></p>
 
 </div>
 
@@ -66,22 +96,22 @@
 
 
 
-	<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="mymodel">
+<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="mymodel">
   <div class="modal-dialog modal-sm">
     <div class="modal-content">
 				<div class="modal-header">
-					<h3 class="text-center text-dark">LOGIN</h3>
+					<h3 class="text-center text-light">LOGIN</h3>
 					<button type="button" class="close" data-dismiss="modal">&times;</button>
                     </div>
                     <div class="modal-body">
                     	 <form action="log_in" method="POST">
                              
                           <div class="form-group">
-                               <br><label>USERNAME</label>
-                               <br><input type="text" name="customer" placeholder="ENTER YOUR NAME" >
+                          <br><i class="fas fa-user-tie fa-2x" style="color:#660033;"></i>
+                               <input type="text" name="customer" placeholder="Your Registered Name" >
 
-                                <br><label>PASSWORD</label>
-                                 <br><input type="password" name="pass" placeholder="ENTER YOUR PASSWORD"  >
+                                <br><i class="fas fa-lock fa-2x" style="color:#660033;"></i>
+                                    <input type="password" name="pass" placeholder="Password"  >
                     </div>
 
 
@@ -102,8 +132,6 @@
 
 
 
-
-
 <!-- Modal -->
 
 
@@ -111,33 +139,36 @@
   <div class="modal-dialog modal-sm">
     <div class="modal-content">
 				<div class="modal-header">
-					<h3 class="text-center text-dark">LOGIN</h3>
+					<h3 class="text-center text-light">CREATE ACCOUNT</h3>
 					<button type="button" class="close" data-dismiss="modal">&times;</button>
                     </div>
         <div class="modal-body">
        <form action="creat_acc" method="POST">
       
            <div class="form-group ">
-               <br><i class="	fas fa-user fa-3x" style="color:#660033;"></i>
-                <br><input type="text" name="user" placeholder="Full name"><br>
+               <br><i class="fas fa-user-tie fa-3x" style="color:#660033;"></i>
+                    <input type="text" name="user" placeholder="Full name"><br>
 
 
-                 <br><label>ADHAAR NUMBER</label>
-                 <input type="file" name="adhar" placeholder="Your Adhaar Nuber"><br>
+                 
+                <br><i class="far fa-map-marker-alt fa-3x " style="color:#660033;"aria-hidden="true"></i>
+                      <input type="text" name="address" placeholder="Your Address" ><br>
 
 
-                <br><label>PAN NUMBER</label>
-                <br><input type="text" name="pan" placeholder="Your Pan Number" maxlength="10"><br>
+                       <br><i class="fas fa-envelope-open-text fa-2x " style="color:#660033;"aria-hidden="true"></i>
+                             <input type="email" name="email" placeholder="Your email"></input><br>
 
-                <i class="fa fa-map-marker " style="color:#660033;"aria-hidden="true"></i>
-                <br><textarea type="text" name="address" placeholder="Your Address" ></textarea><br>
+                <br><i class="fas fa-address-card fa-2x" style="color:#660033;"aria-hidden="true"></i>
+                    <input type="text" name="pan" placeholder=" Pan Number" maxlength="10"><br>
+
+                    <br><i class="far fa-address-card  fa-2x" style="color:#660033;"aria-hidden="true"></i>
+                    <input type="text" name="pan" placeholder=" adhaar Number" maxlength="10"><br>
+
+                    <br><i class="	fas fa-phone-alt fa-2x" style="color:#660033;"aria-hidden="true"></i>
+                        <input type="text" name="contact" placeholder="phone number"  ><br>
 
 
-               <br><label>Email_id</label>
-               <br><input type="email" name="email" placeholder="Your email" ></input><br>
-
-        
-               <label for="image1">IMAGE 1</label>
+                    <br><i class="far fa-image fa-2x"  style="color:#660033;"></i> <label for="image1">Upload Your Photograph</label>
                <input type="file" id="image1" name="pic1" placeholder="image" class="input" required><br>
       
        </div>
@@ -156,36 +187,7 @@
 </div>
 
 
-<script type="text/javascript">
 
-    var username = document.forms["myform"]["user"];
-    var adhar = document.forms["myform"]["adhar"];
-    var pan = document.forms["myform"]["pan"];
-    var address = document.forms["myform"]["address"];
-    var balance = document.forms["myform"]["bal"];
-    var signature = document.forms["myform"]["sign"];
-    var bill = document.forms["myform"]["Bill"];
-
-
-    name=document.getElementById("user_name error");
-    adhar=document.getElementById("adhar");
-    balance=document.getElementById("balance");
-   address=document.getElementById("address");
-    pan=document.getElementById("pan");
-    signature=document.getElementById("sign");
-   bill=document.getElementById("bill");
-
-   function validate()
-   {
-
-     if( username.value==""){
-
-     username.style.border="1ps solid red";
-     name="cus_name is required";
-     return false;
-     }
-
-</script>
 </body>
 </html>
 
